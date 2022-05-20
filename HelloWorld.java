dca7f069da758e4d4ee3ee824d2f4b4845ba66a5
souravjha20 <66384964+souravjha20@users.noreply.github.com>
2022-05-20 21:58:56 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@
 
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World, welcome to Git and Jenkins!"); 
+        System.out.println("Hello, World!"); 
     }
 }
